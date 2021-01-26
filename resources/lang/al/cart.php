65ab5991ae3de6041bffd85d1374133cefc5d97a
mendi19 <armendlatifi90@gmail.com>
2021-01-26 01:29:44 +0100

    Update cart.php

--- resources/lang/al/cart.php
+++ resources/lang/al/cart.php
@@ -2,7 +2,7 @@
 return [
 	'confirm_order_btn'	=> 'KONFIRMO POROSINË',
 	'head_1'	=> 'Të dhënat për porosi',
-	'text_2'	=> 'Të nderuar përdorues, pas bërjes së porosisë do të fitoni konfirmim në mailin që keni lënë për kontakt, për informacione më të hollësishme telefononi në qendrën tonë për kujdes ndaj klientëve të Natura Therapy në numrin +35544803344.<br>
+	'text_2'	=> 'Të nderuar përdorues, pas bërjes së porosisë do të fitoni konfirmim në mailin që keni lënë për kontakt, për informacione më të hollësishme telefononi në qendrën tonë për kujdes ndaj klientëve të Natura Therapy në numrin +383 45 956 000.<br>
 Nëse numri i telefonit ose e-maili që keni lënë nuk ekzistojnë ose nuk janë në dispozicion, porosia e bërë nuk konfirmohet si e bërë.',
 
 'total_price'	=> 'Gjithsej çmimi',
